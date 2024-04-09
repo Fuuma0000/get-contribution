de8bcaafd37524213fb3e17e318e1f86bbc6c529
Fuuma <masa81hiro69@gmail.com>
2024-04-09 12:19:09 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Fuuma0000/get-contribution
+
+go 1.21.4
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+// GitHub API からのレスポンスをマッピングする構造体
+type GithubResponse struct {
+	Data struct {
+		User struct {
+			ContributionsCollection struct {
+				ContributionCalendar struct {
+					Weeks []struct {
+						ContributionDays []struct {
+							ContributionCount int    `json:"contributionCount"`
+							Date              string `json:"date"`
+						} `json:"contributionDays"`
+					} `json:"weeks"`
+				} `json:"contributionCalendar"`
+			} `json:"contributionsCollection"`
+		} `json:"user"`
+	} `json:"data"`
+}
+
+// GitHub API への認証トークン
+var TOKEN string
+
+// GitHub のユーザー名
+var USER = "Fuuma0000"
+
+// GitHub API のエンドポイント URL
+var URL = "https://api.github.com/graphql"
+
+// GraphQL のクエリ
+var QUERY = fmt.Sprintf(`
+{
+  user(login: "%s") {
+    contributionsCollection {
+      contributionCalendar {
+        weeks {
+          contributionDays {
+            contributionCount
+            date
+          }
+        }
+      }
+    }
+  }
+}
+`, USER)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	TOKEN = os.Getenv("GITHUB_API_TOKEN")
+
+	// GitHub API へのリクエストを作成する
+	requestBody, err := json.Marshal(map[string]string{"query": QUERY})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request, err := http.NewRequest("POST", URL, strings.NewReader(string(requestBody)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header.Set("Authorization", fmt.Sprintf("bearer %s", TOKEN))
+
+	// GitHub API へのリクエストを実行する
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// レスポンスを GithubResponse 構造体にマッピングする
+	var githubReponse GithubResponse
+	if err := json.NewDecoder(response.Body).Decode(&githubReponse); err != nil {
+		log.Fatal(err)
+	}
+
+	// 今日のコントリビューション数を取得し、草を生やしていない場合にメッセージを出力する
+	if !isTodayContributed(githubReponse) {
+		fmt.Println("今日の草を生やしていません。")
+		return
+	}
+}
+
+// 今日のコントリビューションがあるかどうかを判定する関数
+func isTodayContributed(response GithubResponse) bool {
+	todayContribution := response.Data.User.ContributionsCollection.ContributionCalendar.Weeks[0].ContributionDays[0]
+	return todayContribution.ContributionCount > 0
+}
